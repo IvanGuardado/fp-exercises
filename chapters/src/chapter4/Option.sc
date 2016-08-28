77d70d81b5c83f105ec4fb41f01f7160199c89fc
Iván Guardado Castro <dev.ivangc@gmail.com>
2016-08-28 22:56:57 +0200

    Finishes the Option implementation

--- chapters/src/chapter4/Option.sc
+++ chapters/src/chapter4/Option.sc
@@ -17,3 +17,18 @@ Option.filter(Some(1))(_ < 10)
 
 Option.orElse(Some(1), Some(2))
 Option.orElse(None, Some(2))
+
+
+Option.mean(Seq(8.0, 9.0, 10.0))
+Option.variance(Seq(1, 2, 3, 4, 5, 6))
+
+Option.map2(Some(1), Some(2))(_ + _)
+Option.map2(None : Option[Int], Some(2))(_ + _)
+Option.map2(Some(1), None: Option[Int])(_ + _)
+
+Option.sequence(List(Some(1), Some(2), Some(3)))
+Option.sequence(List(Some(1), None, Some(3)))
+
+Option.sequence(Nil)
+
+Option.traverse(List(1, 2))(a => if (a > 2) None else Some(a))
--- chapters/src/chapter4/Option.scala
+++ chapters/src/chapter4/Option.scala
@@ -26,4 +26,30 @@ object Option {
   def orElse[A,B>:A](o: Option[A], ob: => Option[B]): Option[B] = getOrElse(map(o)(Option(_)), ob)
 
   def filter[A](o: Option[A])(f: A => Boolean): Option[A] = flatMap(o)(a => if(f(a)) Option(a) else Option())
+
+  def mean(xs: Seq[Double]) : Option[Double] = {
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+  }
+
+  def variance(xs: Seq[Double]) : Option[Double] = {
+    Option.flatMap(mean(xs))(m => {
+      Option(xs.map(x => Math.pow(x - m, 2)).sum / xs.length)
+    })
+  }
+
+  def lift[A,B](f: A => B) : Option[A] => Option[B] = Option.map(_)(f)
+
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A,B) => C) : Option[C] = (a, b) match {
+    case (Some(a), Some(b)) => Option(f(a, b))
+    case (None, _) => None
+    case (_, None) => None
+  }
+
+  def sequence[A](xs: List[Option[A]]): Option[List[A]] = traverse(xs)(a => a)
+
+  def traverse[A,B](xs: List[A])(f: A => Option[B]) : Option[List[B]] = xs.foldRight[Option[List[B]]](Some(Nil))((a, b) => f(a) match {
+    case None => None
+    case Some(value) => Option.flatMap(b)(xs => Some(value :: xs))
+  })
 }
