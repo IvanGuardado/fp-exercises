b2bade629c58a3eb5fae568be14fd4224a73998d
Iván Guardado Castro <dev.ivangc@gmail.com>
2016-09-06 23:02:43 +0200

    Generalizes Rand to State and implements generic functions

---
+++ chapters/src/chapter6/State.sc
@@ -0,0 +1,30 @@
+import chapter6.RNG.Simple
+import chapter6.RNG._
+import chapter6.State
+
+val s = unit(1)
+
+map(s)(a => a + 2)(Simple(2))
+
+nonNegativeInt(Simple(Int.MinValue))
+
+double(Simple(100))
+
+intDouble(Simple(1))
+
+ints(5)(Simple(1))
+
+map2(unit(1), unit(2))(_ + _)(Simple(1))
+
+sequence(List(unit(1), unit(2)))(Simple(1))
+
+flatMap(unit(2))(a => if (a == 1) unit(2) else unit(3))(Simple(1))
+
+State(unit(1)).flatMap(a => if (a == 1) State.unit(2) else State.unit(3)).run(Simple(1))
+
+State(unit(1)).map(_ + 1).run(Simple(100))
+
+State(unit(1)).map2(State(unit(2)))(_+_).run(Simple(100))
+
+val l = List[State[Int, Int]](State.unit(1), State.unit(2))
+State.sequence(l).run(100)
--- chapters/src/chapter6/State.scala
+++ chapters/src/chapter6/State.scala
@@ -22,11 +22,7 @@ object RNG {
   def unit[A](a: A): Rand[A] =
     rng => (a, rng)
 
-  def map[A,B](s: Rand[A])(f: A => B): Rand[B] =
-    rng => {
-      val (a, rng2) = s(rng)
-      (f(a), rng2)
-    }
+  def map[A,B](s: Rand[A])(f: A => B): Rand[B] = flatMap(s)(a => unit(f(a)))
 
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
     val (i, rng2) = rng.nextInt
@@ -64,11 +60,9 @@ object RNG {
   }
 
   def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
-    rng => {
-      val (a, r1) = ra(rng)
-      val (b, r2) = rb(r1)
-      (f(a,b), r2)
-    }
+    flatMap(ra)(a =>
+      flatMap(rb)(b => unit(f(a,b)))
+    )
 
   def both[A,B](ra: Rand[A], rb: Rand[B]): Rand[(A,B)] =
     map2(ra, rb)((_, _))
@@ -92,14 +86,31 @@ object RNG {
   }
 }
 
-//case class State[S,+A](run: S => (A, S)) {
-//  def map[B](f: A => B): State[S, B] =
-//    sys.error("todo")
-//  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
-//    sys.error("todo")
-//  def flatMap[B](f: A => State[S, B]): State[S, B] =
-//    sys.error("todo")
-//}
+case class State[S,+A](run: S => (A, S)) {
+
+  def map[B](f: A => B): State[S, B] = flatMap(a => State.unit(f(a)))
+
+  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+    flatMap(a =>
+      sb.flatMap(b => State.unit(f(a,b)))
+    )
+
+  def flatMap[B](f: A => State[S, B]): State[S, B] = State[S, B](s => {
+      val (i, r) = run(s)
+      f(i).run(r)
+    })
+}
+object State {
+  def unit[S,A](a: A): State[S, A] = State(s => (a, s))
+
+  def sequence[S,A](fs: List[State[S, A]]): State[S, List[A]] = State(rng => {
+    fs.foldRight((Nil : List[A], rng))((rand, b) => {
+      val (i, r2) = rand.run(b._2)
+      (i :: b._1, r2)
+    })
+  })
+
+}
 
 sealed trait Input
 case object Coin extends Input
